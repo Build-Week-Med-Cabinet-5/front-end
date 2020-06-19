27b926ebc77c55010a4f89e2dc08b675521940c0
Matthew Martin <zhintateo19924559@gmail.com>
2020-06-18 21:56:01 -0500

    Pulled in the Login Component

--- front-end-med-cabinet-5/src/App.js
+++ front-end-med-cabinet-5/src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import {Route, Link} from 'react-router-dom';
 import './App.css';
 import UserCreation from './components/UserCreation';
+import Login from "./components/Login";
+import { Container } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <Route exact path='/'>
-        <Link to='/signup'>Sign Up</Link>
+        <Link to='/login'>Log in</Link>
       </Route>
+        <Route exact path='/login'>
+            <Login />
+        </Route>
       <Route exact path='/signup'>
         <UserCreation />
       </Route>
-    </div>
+    </Container>
   );
 }
 
